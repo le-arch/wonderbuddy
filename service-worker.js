72e709e2cb0b279d2e6f0b748fa2208832ec2c21
Leonie Basil <128210955+le-arch@users.noreply.github.com>
2025-08-07 16:41:38 +0100

    Create service-worker.js file

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('wonderbuddy-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
